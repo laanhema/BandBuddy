4ef5dde53614b8370f673bbe5933522a620561a5
Lauri Makkonen <laurimakkonen@hotmail.fi>
2018-01-30 14:49:03 +0000

    lisättiin vaihe 2:sen tiedostot. Vielä täytyy päivittää files.txt

---
+++ src/BandBuddy/BandBuddyController.java
@@ -0,0 +1,16 @@
+package BandBuddy;
+import javafx.fxml.FXML;
+import java.awt.Button;
+
+/**
+ * @author Lauri
+ * @version 25.1.2018
+ *
+ */
+public class BandBuddyController {
+      // TODO
+	
+    @FXML
+    private Button lisaajasenpainike;
+    //
+}
---
+++ src/BandBuddy/BandBuddyMain.java
@@ -0,0 +1,38 @@
+package BandBuddy;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
+
+/**
+ * @author Markus Mäntymaa ja Lauri Makkonen
+ * @version 30.01.2018
+ *
+ */
+public class BandBuddyMain extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader ldr = new FXMLLoader(getClass().getResource("paakentta.fxml"));
+            final Pane root = ldr.load();
+            //final BandBuddyController bandbuddyCtrl = (BandBuddyController) ldr.getController();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("bandbuddy.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("BandBuddy");
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args Ei käytössä
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
